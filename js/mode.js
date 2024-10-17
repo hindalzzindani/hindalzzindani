a7ea3883b41de16291450179f9bede8293985eb8
hindalzzindani <hindalzzindani@gmail.com>
2024-10-17 11:40:01 +0300

    web site

---
+++ js/mode.js
@@ -0,0 +1,11 @@
+// ***** dark mod
+
+console.log('toggle ');
+
+
+const checkbox = document.getElementById('toggle-checkout');
+
+checkbox.addEventListener('change', () => {
+    document.body.classList.toggle('light');
+})
+// ***end dark mod
